398a80fc621a5a8cc6903c9cc443fc454d57193c
Jason Shiao <b01901105@ntu.edu.tw>
2022-02-25 16:18:24 +0800

    Fix file not copied when copying project

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -23,7 +23,7 @@
 #include "usbd_cdc_if.h"
 
 /* USER CODE BEGIN INCLUDE */
-
+#include "serial.h"
 /* USER CODE END INCLUDE */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -263,6 +263,11 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
   /* USER CODE BEGIN 6 */
+  int i;  
+  for (i = 0; i < *Len; i++) {
+    serial_handle_data(Buf[i]);
+  }
+
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
   return (USBD_OK);
