6b3d1e1e39d5ed7d4ad6cbb4a8b497f71689ed04
Jason Shiao <b01901105@ntu.edu.tw>
2022-04-13 18:14:43 +0800

    Workaround for delay_ms() hang inside serial interrupt

--- Core/Src/stepper.c
+++ Core/Src/stepper.c
@@ -285,7 +285,7 @@ void st_go_idle()
   if (((settings.stepper_idle_lock_time != 0xff) || sys_rt_exec_alarm || sys.state == STATE_SLEEP) && sys.state != STATE_HOMING) {
     // Force stepper dwell to lock axes for a defined amount of time to ensure the axes come to a complete
     // stop and not drift from residual inertial forces at the end of the last movement.
-    delay_ms(settings.stepper_idle_lock_time);
+    //delay_ms(settings.stepper_idle_lock_time);
     pin_state = true; // Override. Disable steppers.
   }
   if (bit_istrue(settings.flags,BITFLAG_INVERT_ST_ENABLE)) { pin_state = !pin_state; } // Apply pin invert.
